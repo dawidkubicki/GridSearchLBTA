cea6d3411e8a6a7c7611e9b4e9f4deecb5bfbf1b
Dawid Kubicki <dawid@pluscode.io>
2020-11-21 12:29:22 +0100

    Initial Commit

---
+++ GridSearchLBTA/GridSearchLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchLBTAApp.swift
+//  GridSearchLBTA
+//
+//  Created by Dawid Kubicki on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
