37dfde95b31f81051fe426b64538e973ee0c8129
Dawid Kubicki <dawid@pluscode.io>
2020-11-21 13:30:53 +0100

    grid template added

--- GridSearchLBTA/ContentView.swift
+++ GridSearchLBTA/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200))
+                ],spacing: 10, content: {
+                    
+                    ForEach(0..<20, id: \.self) { num in
+                        ItemView()
+                    }
+                    
+                }).padding(.horizontal, 12)
+            }.navigationTitle("Grid Search")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ItemView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Spacer()
+                .frame(width: 100, height: 100)
+                .background(Color.blue)
+            Text("App title")
+                .font(.system(size: 10, weight: .semibold))
+            Text("Release Date")
+                .font(.system(size: 9, weight: .regular))
+            Text("Copyright")
+                .font(.system(size: 9, weight: .regular))
+                .foregroundColor(.gray)
+            
+        }
+        .padding()
+        .background(Color.red)
     }
 }
