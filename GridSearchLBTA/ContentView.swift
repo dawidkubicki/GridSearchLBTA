4ad9119b0f919edbcfaa7c20cc658a7fdeaf74ea
Dawid Kubicki <dawid@pluscode.io>
2020-11-21 16:44:15 +0100

    grid alpha ready

--- GridSearchLBTA/ContentView.swift
+++ GridSearchLBTA/ContentView.swift
@@ -6,19 +6,67 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
+
+struct RSS: Decodable{
+    let feed: Feed
+}
+
+struct Feed:Decodable {
+    let results: [Result]
+}
+
+struct Result:Decodable, Hashable {
+    let copyright, name, artworkUrl100, releaseDate: String
+}
+
+
+
+class GridViewModel: ObservableObject {
+    
+    @Published var items = 0..<5
+    @Published var results = [Result]()
+    
+    init() {
+        // json decoding simulation
+//        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (_) in
+//            self.items = 0..<15
+            guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/pl/ios-apps/top-free/all/10/explicit.json") else {
+                return
+            }
+            URLSession.shared.dataTask(with: url) { (data, resp, error) in
+                // check error response status and err
+                guard let data = data else { return }
+            do {
+                let rss = try JSONDecoder().decode(RSS.self, from: data)
+                print(rss)
+                DispatchQueue.main.async {
+                    self.results = rss.feed.results
+                }
+            } catch {
+                print("Failed to decode: \(error)")
+            }
+                
+    }.resume()
+    }
+}
+
 
 struct ContentView: View {
+    
+    @ObservedObject var vm = GridViewModel()
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: [
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
-                    GridItem(.flexible(minimum: 100, maximum: 200))
-                ],spacing: 10, content: {
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                    GridItem(.flexible(minimum: 100, maximum: 200), alignment: .top)
+                ], alignment: .leading,spacing: 16, content: {
                     
-                    ForEach(0..<20, id: \.self) { num in
-                        ItemView()
+                    ForEach(vm.results, id: \.self) { app in
+                        ItemView(appTitle: app.name, appReleaseDate: app.releaseDate, appCopyright: app.copyright, appImage: app.artworkUrl100)
                     }
                     
                 }).padding(.horizontal, 12)
@@ -34,21 +82,28 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct ItemView: View {
+    
+    var appTitle: String
+    var appReleaseDate: String
+    var appCopyright: String
+    var appImage: String
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Spacer()
-                .frame(width: 100, height: 100)
-                .background(Color.blue)
-            Text("App title")
+        VStack(alignment: .leading, spacing: 4) {
+    
+            KFImage(URL(string: appImage))
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(22.0)
+    
+            Text(appTitle)
                 .font(.system(size: 10, weight: .semibold))
-            Text("Release Date")
+            Text(appReleaseDate)
                 .font(.system(size: 9, weight: .regular))
-            Text("Copyright")
+            Text(appCopyright)
                 .font(.system(size: 9, weight: .regular))
                 .foregroundColor(.gray)
-            
+            Spacer()
         }
-        .padding()
-        .background(Color.red)
     }
 }
